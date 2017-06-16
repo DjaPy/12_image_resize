97159dfa066ed25482055be5614f1158c77c98c9
Ivanov Sergey <djapy@yandex.ru>
2017-06-16 12:35:49 +0300

    create func get_original_image_size and get_proportional_size

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,28 @@
+from PIL import Image
+
+
+def get_original_image_size(path_to_original):
+    original_image = Image.open(path_to_original)
+    size_image = original_image.size
+    return size_image
+
+
+def get_proportional_size(size_image, width=None, height=None):
+    if height is None:
+        height = (width * size_image[1]) / size_image[0]
+    elif width is None:
+        width = (height * size_image[0]) / size_image[1]
+    return width, height
 
 
 def resize_image(path_to_original, path_to_result):
-    pass
+    if width or height in ()
+    image_resize = get_original_image_size(path_to_original).resize()
 
 
 if __name__ == '__main__':
-    pass
+    path_to_original = input('Enter the path for the image: ')
+    width = int(input('Enter the width of the final image: '))
+    height = int(input('Enter the height of the final image: '))
+    scale = int(input('Specify the scale of the final image: '))
+    path_to_result = input('Specify the path to the final file: ')
